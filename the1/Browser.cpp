1b5bd7d98b35fce2151a39ea9e77c50033da3d5d
mrrsff <esbaltaci123@gmail.com>
2022-10-31 23:02:22 +0300

    continue

--- the1/Browser.cpp
+++ the1/Browser.cpp
@@ -2,23 +2,27 @@
 
 Browser::Browser() {
     // TODO
-    
+    windows = LinkedList<Window>();
 }
 
 void Browser::newWindow() {
     // TODO
+    windows.prepend(Window());
 }
 
 void Browser::closeWindow() {
     // TODO
+    windows.removeNodeAtIndex(0);
 }
 
 void Browser::switchToWindow(int index) {
     // TODO
+    windows.moveToIndex(0, index);
 }
 
 void Browser::moveTab(Window &window1, Window &window2) {
     // TODO
+    
 }
 
 void Browser::mergeWindows(Window &source, Window &destination) {
